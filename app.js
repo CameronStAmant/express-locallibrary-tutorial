adf49e5b9daf5f3d7791f020a94e4a59b7d744e9
Cameron St. Amant <cameronstamant@protonmail.com>
2021-03-27 15:54:36 -0600

    add more prod setup

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var helmet = require('helmet');
 var app = express();
 
 var mongoose = require('mongoose');
-var mongoDB =
+var dev_db_url =
   'mongodb+srv://local_library_user_1:PocbkUcdC3H3yFkX7dFf9932qifpD7oWKqRXhvrzxEgForV@cluster0.fevtv.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
